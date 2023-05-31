0a4837424fba4aa43ac8644dddc6a2cbc9927226
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-05-31 17:16:25 -0400

    Steps dummy automatizacion inicio de sesion

---
+++ AutomationBDD/features/step_definitions/homepage/inicio_de_sesion/inicio_de_sesion_credencialesSteps.rb
@@ -0,0 +1,31 @@
+Given('yo accedo a la pagina de MercuryTours') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('accedo al enlace de iniciar sesion') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('ingreso un usuario y contraseña correctos') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('presiono el boton {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('deberia ver el mensaje {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('ver el boton de cerrar sesion') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('ingreso un usuario y contraseña incorrectos') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('estoy en la pagina principal') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
