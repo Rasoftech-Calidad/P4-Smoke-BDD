60c5055aca8dd531c16666557a7a99b5c8fd7f69
ASDIO1 <diego.an.sanchez.a@gmail.com>
2023-06-02 10:43:31 -0400

    Escenario modificado y acceder a signon

--- AutomationBDD/features/modules/inicio_de_sesion/inicio_de_sesion_credenciales.feature
+++ AutomationBDD/features/modules/inicio_de_sesion/inicio_de_sesion_credenciales.feature
@@ -6,22 +6,27 @@ Feature: Inicio De Sesion Con Credenciales
 Background:
     Given yo accedo a la pagina de MercuryTours
 
-Scenario: Iniciar sesion con credenciales correctos desde pagina de inicio de sesion
-    When accedo al enlace de iniciar sesion
-    And ingreso un usuario y contraseña correctos
+@actual
+Scenario Outline: Iniciar sesion exitosamente desde pagina de inicio de sesion
+    When accedo al enlace de "Sign-On" para iniciar sesion
+    And ingreso un <Usuario> y <Contraseña> correctos
     And presiono el boton "Submit"
     Then deberia ver el mensaje "Login Succesfully"
     And ver el boton de cerrar sesion
+Examples:
+    | Usuario | Contraseña |
+    | hola | hola    |
+    | hola | chau    |
 
 Scenario: Iniciar sesion con credenciales incorrectos desde pagina de inicio de sesion
-    When accedo al enlace de iniciar sesion
+    When accedo al enlace de "SIGN-ON" para iniciar sesion
     And ingreso un usuario y contraseña incorrectos
     And presiono el boton "Submit"
     Then deberia ver el mensaje "Enter your userName and password correct"
         
 
 Scenario: Iniciar sesion con credenciales correctos desde la pagina principal
-    When estoy en la pagina principal
+    When quiero iniciar sesion desde los campos de entrada en la pagina principal de MercuryTours
     And ingreso un usuario y contraseña correctos
     And presiono el boton "Submit"
     Then deberia ver el mensaje "Login Succesfully"
--- AutomationBDD/features/step_definitions/inicio_de_sesion/inicio_de_sesion_credencialesSteps.rb
+++ AutomationBDD/features/step_definitions/inicio_de_sesion/inicio_de_sesion_credencialesSteps.rb
@@ -1,31 +1,15 @@
+# Given yo accedo a la pagina de MercuryTours
 Given('yo accedo a la pagina de MercuryTours') do
-    visit 'http://demo.guru99.com/'
+    visit 'http://demo.guru99.com/test/newtours/'
 end
 
-When('accedo al enlace de iniciar sesion') do
-    pending # Write code here that turns the phrase above into concrete actions
+# When accedo al enlace de "SIGN-ON" para iniciar sesion
+When(/^accedo al enlace de "([^"]*)" para iniciar sesion$/) do |signon|
+    css = 'body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td:nth-child(1) > a'
+    find(:css, css).click
+    #click_link(signon)
 end
 
-When('ingreso un usuario y contraseña correctos') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-When('presiono el boton {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('deberia ver el mensaje {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('ver el boton de cerrar sesion') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-When('ingreso un usuario y contraseña incorrectos') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-When('estoy en la pagina principal') do
+When('ingreso un <User Name> y <Password> correctos') do
     pending # Write code here that turns the phrase above into concrete actions
 end
