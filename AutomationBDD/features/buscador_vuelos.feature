40309fcea79ac3029866dfeb98abb8dac7047afc
Alfred <alfred.garcia@ucb.edu.bo>
2023-06-02 08:53:21 -0400

    Escenario #3 AUTOMATIZADO - FLIGHTS

--- AutomationBDD/features/buscador_vuelos.feature
+++ AutomationBDD/features/buscador_vuelos.feature
@@ -18,3 +18,17 @@ Scenario: regresar a la pagina de "MercuryTours" cuando falla la reserva de un v
 Scenario: reservar un vuelo por defecto
     When presionar la imagen de boton "Continue->"
     Then muestra el mensaje "After flight finder - No Seats Avaialble"
+
+@maximize
+Scenario: Buscar vuelo no valido debido a fecha de salida adelantada a la fecha de llegada
+    When ingrese los campos requeridos como se muestra a continuación para el vuelo
+      | Type:           | Round Trip     |
+      | Passengers:     | 1              |
+      | Departing form: | Acapulco       |
+      | On:             | February 1     |
+      | Arriving In:    | Acapulco       |
+      | Returning:      | January 1      | 
+      | Service Class:  | Business class |
+      | Airline:        | No Preference  |
+      And presionar la imagen de boton "Continue->"
+    Then muestra el mensaje "After flight finder - No Seats Avaialble"
--- AutomationBDD/features/step_definitions/buscador_vuelos.rb
+++ AutomationBDD/features/step_definitions/buscador_vuelos.rb
@@ -15,4 +15,36 @@ end
         find(:xpath, xpath).click
     end
 
-    
+#When ingrese los campos requeridos como se muestra a continuación para el vuelo
+When('ingrese los campos requeridos como se muestra a continuación para el vuelo') do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+      case key
+      when 'Type:'
+        radio_button = find('[value="oneway"]') if value.downcase == 'one way'
+        radio_button = find('[value="roundtrip"]') if value.downcase == 'round trip'
+        radio_button.choose
+      when 'Passengers:'
+        select value, from: 'passCount'
+      when 'Departing From:'
+        select value, from: 'fromPort'
+      when 'On:'
+        value_date = value.split
+        select value_date[0], from: 'fromMonth'
+        select value_date[1], from: 'fromDay'
+      when 'Arriving In:'
+        select value, from: 'toPort'
+      when 'Returning:'
+        value_date = value.split
+        select value_date[0], from: 'toMonth'
+        select value_date[1], from: 'toDay'
+      when 'Service Class:'
+        radio_button = find('[value="Coach"]') if value.downcase == 'economy class'
+        radio_button = find('[value="Business"]') if value.downcase == 'business class'
+        radio_button = find('[value="First"]') if value.downcase == 'first class'
+        radio_button.choose
+      when 'Airline:'
+        select value, from: 'airline'
+      end
+    end
+end
