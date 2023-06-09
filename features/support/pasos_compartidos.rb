148b0bc8a0990324fae9ef5564662636832856ff
Alfred <alfred.garcia@ucb.edu.bo>
2023-06-09 18:06:24 -0400

    Pasos compartidos de inicio de sesion y vacaciones

--- features/support/pasos_compartidos.rb
+++ features/support/pasos_compartidos.rb
@@ -9,7 +9,7 @@ When(/^acceder al enlace "([^"]*)" del menu de opciones$/) do |linkText|
   click_link(linkText)
 end
 
-# contacto, itinerarios, perfil, soporte, registrarse, sign-on
+# contacto, itinerarios, perfil, soporte, registrarse, sign-on, vacaciones
 When(/^acceder al enlace "([^"]*)" de la barra de navegacion$/) do |linkText|
   click_link(linkText)
 end
@@ -40,4 +40,19 @@ end
 Then(/^presionar la imagen de boton "Continue->" para continuar$/) do
   xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td/input'
   find(:xpath, xpath).click
+end
+
+# Iniciar sesion exitosamente desde pagina de inicio de sesion
+# Acceder a la pagina de vacaciones como un usuario ya registrado
+And('ingreso un {string} y {string} correctos') do |string, string2|
+  username_input = 'body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > input'
+  password_input = 'body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=password]'
+  find(:css, username_input).fill_in with: string
+  find(:css, password_input).fill_in with: string2
+end
+
+# TODOS LOS ESCENARIOS DE inicio_de_sesion_credenciales
+# Acceder a la pagina de vacaciones como un usuario ya registrado
+And('presiono el boton {string}') do |string|
+  click_button(string)
 end
