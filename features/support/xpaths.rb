323aa9205d1df63c3b456efdaa832bff4000943c
Alfred <alfred.garcia@ucb.edu.bo>
2023-06-14 12:27:59 -0400

    Hot Fix

--- features/support/xpaths.rb
+++ features/support/xpaths.rb
@@ -3,7 +3,7 @@ And(/^presionar la imagen de boton "BACK TO HOME" para volver$/) do
     xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[4]/td/a/img'
     find(:xpath, xpath).click
 end
-  
+
 # itinerarios, perfil, vuelos
 And(/^presionar la imagen de boton "BACK TO HOME" para volver desde la barra de navegacion$/) do
     xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td/a/img'   
